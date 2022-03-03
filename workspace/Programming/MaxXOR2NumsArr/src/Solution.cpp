25194ea501d35775306f3b94b440c1ae4898c280
Iyer <yegnesh.s.iyer@intel.com>
2022-03-03 15:42:06 +0530

    Leetcode -- 20220303 -- 2
    Updating the maxxor while adding a new number into the trie.
    Optimization - no need to re-traverse the trie to find out
    the max.
    
    Signed-off-by: Yegnesh <yegnesh22@gmail.com>

--- workspace/Programming/MaxXOR2NumsArr/src/Solution.cpp
+++ workspace/Programming/MaxXOR2NumsArr/src/Solution.cpp
@@ -1,23 +1,76 @@
 /*
  * Solution.cpp
  *
- *  Created on: Jan 27, 2022
+ *  Created on: Mar 3, 2022
  *      Author: ysiyer
  */
 
 #include <iostream>
-#include <climits>
 #include <vector>
-#include <map>
+#include <bitset>
+
 
 using namespace std;
+
+#define NUMBITS 32
+
+struct BitWiseTrie {
+	BitWiseTrie* bit[2] = {nullptr};
+};
+
+class Solution {
+private:
+	BitWiseTrie* m_root;
+public:
+	int insert(int n);
+	int findMaximumXOR(vector<int>& nums);
+};
+
+int Solution::insert(int n)
+{
+	BitWiseTrie* t = m_root;
+	BitWiseTrie* mx = m_root;
+	bitset<NUMBITS> XOR(0);
+	bitset<NUMBITS> x(n);
+
+	for (int i = (NUMBITS - 1); i >= 0; i--) {
+		if (t->bit[x[i]] == nullptr)
+			t->bit[x[i]] = new BitWiseTrie();
+		if (mx->bit[!x[i]] != nullptr) {
+			XOR[i] = 1;
+			mx = mx->bit[!x[i]];
+		} else
+			mx = mx->bit[x[i]];
+
+		t = t->bit[x[i]];
+	}
+
+	return (int)XOR.to_ulong();
+}
+
+int Solution::findMaximumXOR(vector<int>& nums)
+{
+	int thisXOR;
+	int maxXOR = 0;
+	int nl = nums.size();
+
+	m_root = new BitWiseTrie();
+
+	for (int i = 0; i < nl; i++) {
+		thisXOR = insert(nums[i]);
+		maxXOR = max(thisXOR, maxXOR);
+	}
+
+	return maxXOR;
+}
+
+#define result(x) ((x.ao == x.eo) ? "pass" : "fail")
 
 struct ti {
 	vector<int> nums;
 	int eo;
 	int ao;
 };
-
 
 vector<ti> t = {
 		{
@@ -28,104 +81,17 @@ vector<ti> t = {
 				.nums = {14,70,53,83,49,91,36,80,92,51,66,70},
 				.eo = 127,
 		},
-		{
-				.nums = {2147483647,2147483646,2147483645},
-				.eo = 3,
-		},
 };
-
-class Solution {
-public:
-	virtual int findMaximumXOR(vector<int>& nums) = 0;
-};
-
-class BasicSolution: public Solution {
-public:
-    int findMaximumXOR(vector<int>& nums) {
-    	int r = 0;
-
-    	for (int i = 0; i < nums.size(); i++) {
-    		for (int j = i + 1; j < nums.size(); j++) {
-    			r = max(r, nums[i] ^ nums[j]);
-    		}
-    	}
-
-    	return r;
-    }
-};
-
-#define NUM_BITS 32
-
-struct BitWiseTrie {
-	BitWiseTrie* bit[2];
-	BitWiseTrie() {
-		bit[0] = nullptr;
-		bit[1] = nullptr;
-	}
-};
-
-class Trie: public Solution {
-private:
-	BitWiseTrie* TrieRoot;
-	void insert(int x);
-	int maxXOR(int n);
-public:
-	int findMaximumXOR(vector<int>& nums);
-};
-
-void Trie::insert(int x)
-{
-	BitWiseTrie* t = TrieRoot;
-	bitset<NUM_BITS> b(x);
-
-	for (int i = NUM_BITS-1; i >= 0; i--) {
-		if (t->bit[b[i]] == nullptr)
-			t->bit[b[i]] = new BitWiseTrie();
-		t = t->bit[b[i]];
-	}
-}
-
-int Trie::maxXOR(int n)
-{
-	BitWiseTrie* t = TrieRoot;
-	bitset<NUM_BITS> b(n);
-	bitset<NUM_BITS> mXOR(0);
-
-	for (int i = NUM_BITS-1; i >= 0; i--) {
-		if (t->bit[!b[i]] != nullptr) {
-			t = t->bit[!b[i]];
-			mXOR[i] = 1;
-		} else
-			t = t->bit[b[i]];
-	}
-	return (int)mXOR.to_ulong();
-}
-
-int Trie::findMaximumXOR(vector<int>& nums)
-{
-	int ns = nums.size();
-	int mXOR = 0;
-
-	// Create the trie
-	TrieRoot = new BitWiseTrie();
-	for (int i = 0; i < ns; i++)
-		insert(nums[i]);
-
-	for (int i = 0; i < ns; i++)
-		mXOR = max(mXOR, maxXOR(nums[i]));
-
-	return mXOR;
-}
 
 int main()
 {
-	//Solution *s = new BasicSolution();
-	Solution *s = new Trie();
+	Solution* s = new Solution();
 
 	for (int i = 0; i < t.size(); i++) {
 		t[i].ao = s->findMaximumXOR(t[i].nums);
 		cout << t[i].ao << endl;
-		cout << i << " -- r: " << ((t[i].ao == t[i].eo) ? "pass": "fail") << endl;
+		cout << i << " -- " << result(t[i]) << endl;
 	}
+
 	return 0;
 }
